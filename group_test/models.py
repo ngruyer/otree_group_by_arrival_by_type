a94b862729b5916a12faca3bf98f06b28f86ed57
N. Gruyer <nicolas@lud.io>
2017-04-23 13:58:59 +0200

    Update models.py

--- group_test/models.py
+++ group_test/models.py
@@ -8,6 +8,7 @@ author = 'Nicolas Gruyer - Economics Games. https://economics-games.com'
 
 doc = """
 Test Game: Players entering the game should be grouped by 4 after their arrival, as soon as possible. But each group of 4 must contain exactly 2 type_1 and 2 type_2 players.
+Be careful, do not manipulate groups containing players who have already entered the round: This might create problems.
 """
 
 
